a2b32c9703ec8383ec9503563ab0e79d2a3cc273
rahul singh <rahulps.singh1@gmail.com>
2020-08-31 11:15:49 +0530

    jhjh

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -10,6 +10,9 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 const port = process.env.PORT || 5000;
 // start express server on port 5000
 app.listen(port, () => {
